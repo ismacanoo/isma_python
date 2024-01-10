0de17e75b5713b0759d8be38c5ebc9f6d6e71267
ismacanoo <06ismael.cano@inscastellet.cat>
2024-01-10 19:18:40 +0100

    comentari alex

--- exercici3.py
+++ exercici3.py
@@ -36,7 +36,7 @@ print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros.")
 print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas.")
 
 separador(4)
-
+#comentari de l'alex
 
 separador(5)
 
